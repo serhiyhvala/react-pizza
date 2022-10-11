0b1792f9426e8ea5838a591a03b83e90204dc5b5
serhiy hvala <serhiyhvaladew@gmail.com>
2022-10-11 13:49:22 +0300

    ️☄️ Create Pizza Block

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import './styles/app.scss'
-function App() {
-  return (
-      <>
+import './assets/styles/app.scss'
+import Header from "./components/Header/Header";
+import Categories from "./components/Categories/Categories";
+import Sort from "./components/Sort/Sort";
+import pizzas from './assets/data/pizzas.json'
+import PizzaBlock from "./components/PizzaBlock/PizzaBlock";
 
-      </>
-  );
+function App() {
+    return (
+        <>
+            <div className="wrapper">
+                <Header/>
+                <div className="content">
+                    <div className="container">
+                        <div className="content__top">
+                            <Categories/>
+                            <Sort/>
+                        </div>
+                        <h2 className="content__title">All Pizzas</h2>
+                        <div className="content__items">
+                            {
+                                pizzas.map((obj) => (
+                                    <PizzaBlock {...obj}/>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
