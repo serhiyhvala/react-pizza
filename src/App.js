eae6571885dfd8a83899f87ad5d687870b35ad6c
serhiy hvala <serhiyhvaladew@gmail.com>
2022-10-11 14:46:31 +0300

    ️🚨️ Fix Key in App Component

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
                         <div className="content__items">
                             {
                                 pizzas.map((obj) => (
-                                    <PizzaBlock {...obj}/>
+                                    <PizzaBlock key={obj.id} {...obj}/>
                                 ))
                             }
                         </div>
