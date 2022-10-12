3c96f0ac363ed03527353a55233004f58d9918d4
serhiy hvala <serhiyhvaladew@gmail.com>
2022-10-12 12:18:52 +0300

    ➕️ Add Prettier Config To Project

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
 import './assets/styles/app.scss'
-import Header from "./components/Header/Header";
-import Categories from "./components/Categories/Categories";
-import Sort from "./components/Sort/Sort";
-import PizzaBlock from "./components/PizzaBlock/PizzaBlock";
-import {useEffect, useState} from "react";
+import Header from './components/Header/Header'
+import Categories from './components/Categories/Categories'
+import Sort from './components/Sort/Sort'
+import PizzaBlock from './components/PizzaBlock/PizzaBlock'
+import { useEffect, useState } from 'react'
 
 function App() {
-    const [items, setItems] = useState([])
-    useEffect(() => {
-        fetch('https://63455cb939ca915a69fcd328.mockapi.io/items')
-            .then((res) => res.json())
-            .then((json) => setItems(json))
-    }, [])
-    return (
-        <>
-            <div className="wrapper">
-                <Header/>
-                <div className="content">
-                    <div className="container">
-                        <div className="content__top">
-                            <Categories/>
-                            <Sort/>
-                        </div>
-                        <h2 className="content__title">All Pizzas</h2>
-                        <div className="content__items">
-                            {
-                                items.map((obj) => (
-                                    <PizzaBlock key={obj.id} {...obj}/>
-                                ))
-                            }
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+	const [items, setItems] = useState([])
+	useEffect(() => {
+		fetch('https://63455cb939ca915a69fcd328.mockapi.io/items')
+			.then(res => res.json())
+			.then(json => setItems(json))
+	}, [])
+	return (
+		<>
+			<div className='wrapper'>
+				<Header />
+				<div className='content'>
+					<div className='container'>
+						<div className='content__top'>
+							<Categories />
+							<Sort />
+						</div>
+						<h2 className='content__title'>All Pizzas</h2>
+						<div className='content__items'>
+							{items.map(obj => (
+								<PizzaBlock key={obj.id} {...obj} />
+							))}
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
 
-export default App;
+export default App
--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -1,43 +1,41 @@
-import React, {useState} from 'react';
-import {sort} from '../../assets/data/sort'
-import sortLabel from "../../assets/img/sortLabel.svg"
+import React, { useState } from 'react'
+import { sort } from '../../assets/data/sort'
+import sortLabel from '../../assets/img/sortLabel.svg'
 
 const Sort = () => {
-    const [visiblePopUp, setVisiblePopUp] = useState(false)
-    const [pizzaSort, setPizzaSort] = useState(0)
+	const [visiblePopUp, setVisiblePopUp] = useState(false)
+	const [pizzaSort, setPizzaSort] = useState(0)
 
-    const onClickSetItem = (i) => {
-        setPizzaSort(i)
-        setVisiblePopUp(false)
-    }
-    return (
-        <div className="sort">
-            <div className="sort__label">
-                <img src={sortLabel} alt=""/>
-                <b>Sort by:</b>
-                <span onClick={() => setVisiblePopUp(!visiblePopUp)}>{sort[pizzaSort]}</span>
-            </div>
-            {
-                visiblePopUp && (
-                    <div className="sort__popup">
-                        <ul>
-                            {
-                                sort.map((sort, i) => (
-                                    <li
-                                        onClick={() => onClickSetItem(i)}
-                                        className={pizzaSort === i ? 'active' : ''}
-                                        key={i}
-                                    >
-                                        {sort}
-                                    </li>
-                                ))
-                            }
-                        </ul>
-                    </div>
-                )
-            }
-        </div>
-    );
-};
+	const onClickSetItem = i => {
+		setPizzaSort(i)
+		setVisiblePopUp(false)
+	}
+	return (
+		<div className='sort'>
+			<div className='sort__label'>
+				<img src={sortLabel} alt='' />
+				<b>Sort by:</b>
+				<span onClick={() => setVisiblePopUp(!visiblePopUp)}>
+					{sort[pizzaSort]}
+				</span>
+			</div>
+			{visiblePopUp && (
+				<div className='sort__popup'>
+					<ul>
+						{sort.map((sort, i) => (
+							<li
+								onClick={() => onClickSetItem(i)}
+								className={pizzaSort === i ? 'active' : ''}
+								key={i}
+							>
+								{sort}
+							</li>
+						))}
+					</ul>
+				</div>
+			)}
+		</div>
+	)
+}
 
-export default Sort;
+export default Sort
