688dafbe3d22e0ea44db6717a9939c2ca124cc42
Arthur Sereda <neonbetcy@gmail.com>
2022-10-14 19:20:17 +0300

    Delete unnecessary files (пояснення: sortList та categories юзаються лише в одному компоненті, тобі ж, для прикладу, sortList не юзається в 2-ох компонентах, лише в одному, так само з categories, тому непотрібно виносити в окремий файл. По-перше, в імпорті дуже негарно виглядає: import { sortList } from '../../assets/data/sortList', ну і по-друге, в тебе створено, без реальної потреби додатковий файл)

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,22 +1,48 @@
 import React from 'react'
-import { categories } from '../../assets/data/categories'
 
-const Categories = ({ value, onClickCategory }) => {
-	return (
-		<div className='categories'>
-			<ul>
-				{categories.map((category, i) => (
-					<li
-						onClick={() => onClickCategory(i)}
-						className={value === i ? 'active' : ''}
-						key={category.id}
-					>
-						{category.name}
-					</li>
-				))}
-			</ul>
-		</div>
-	)
+const categories = [
+  {
+    name: 'All',
+    id: 0
+  },
+  {
+    name: 'Meat',
+    id: 1
+  },
+  {
+    name: 'Vegetarian',
+    id: 2
+  },
+  {
+    name: 'Grill',
+    id: 3
+  },
+  {
+    name: 'Spicy',
+    id: 4
+  },
+  {
+    name: 'Closed',
+    id: 5
+  },
+]
+
+const Categories = ({value, onClickCategory}) => {
+  return (
+      <div className='categories'>
+        <ul>
+          {categories.map((category, i) => (
+              <li
+                  onClick={() => onClickCategory(i)}
+                  className={value === i ? 'active' : ''}
+                  key={category.id}
+              >
+                {category.name}
+              </li>
+          ))}
+        </ul>
+      </div>
+  )
 }
 
 export default Categories
--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -1,42 +1,68 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import sortLabel from '../../assets/img/sortLabel.svg'
-import { useDispatch, useSelector } from 'react-redux'
-import { sortList } from '../../assets/data/sortList'
-import { setSort } from '../../redux/slices/fliterSlice'
+import {useDispatch, useSelector} from 'react-redux'
+import {setSort} from '../../redux/slices/fliterSlice'
+
+const sortList = [
+  {
+    name: 'popularity (DESC)',
+    sortType: 'rating'
+  },
+  {
+    name: 'popularity (ASC)',
+    sortType: '-rating'
+  },
+  {
+    name: 'price (DESC)',
+    sortType: 'price'
+  },
+  {
+    name: 'price (ASC)',
+    sortType: '-price'
+  },
+  {
+    name: 'alphabet (DESC)',
+    sortType: 'title'
+  },
+  {
+    name: 'alphabet (ASC)',
+    sortType: '-title'
+  }
+]
 
 const Sort = () => {
-	const dispatch = useDispatch()
-	const sort = useSelector(state => state.filter.sort)
-	const [visiblePopUp, setVisiblePopUp] = useState(false)
+  const dispatch = useDispatch()
+  const sort = useSelector(state => state.filter.sort)
+  const [visiblePopUp, setVisiblePopUp] = useState(false)
 
-	const onClickSetItem = obj => {
-		dispatch(setSort(obj))
-		setVisiblePopUp(false)
-	}
-	return (
-		<div className='sort' onClick={() => setVisiblePopUp(!visiblePopUp)}>
-			<div className='sort__label'>
-				<img src={sortLabel} alt='' />
-				<b>Sort by:</b>
-				<span onClick={() => setVisiblePopUp(!visiblePopUp)}>{sort.name}</span>
-			</div>
-			{visiblePopUp && (
-				<div className='sort__popup'>
-					<ul>
-						{sortList.map((obj, i) => (
-							<li
-								onClick={() => onClickSetItem(obj)}
-								className={sort.name === obj.name ? 'active' : ''}
-								key={i}
-							>
-								{obj.name}
-							</li>
-						))}
-					</ul>
-				</div>
-			)}
-		</div>
-	)
+  const onClickSetItem = obj => {
+    dispatch(setSort(obj))
+    setVisiblePopUp(false)
+  }
+  return (
+      <div className='sort' onClick={() => setVisiblePopUp(!visiblePopUp)}>
+        <div className='sort__label'>
+          <img src={sortLabel} alt=''/>
+          <b>Sort by:</b>
+          <span onClick={() => setVisiblePopUp(!visiblePopUp)}>{sort.name}</span>
+        </div>
+        {visiblePopUp && (
+            <div className='sort__popup'>
+              <ul>
+                {sortList.map((obj, i) => (
+                    <li
+                        onClick={() => onClickSetItem(obj)}
+                        className={sort.name === obj.name ? 'active' : ''}
+                        key={i}
+                    >
+                      {obj.name}
+                    </li>
+                ))}
+              </ul>
+            </div>
+        )}
+      </div>
+  )
 }
 
 export default Sort
