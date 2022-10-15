6cf0808b2d868ad5ab88a310d77689697de09306
serhiy hvala <serhiyhvaladew@gmail.com>
2022-10-15 14:49:12 +0300

    Update Search with lodash.debounce and useCallback

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,16 +1,32 @@
-import React, { useContext, useRef } from 'react'
+import React, { useCallback, useContext, useRef, useState } from 'react'
+import debounce from 'lodash.debounce'
 
 import { SearchContext } from '../../App'
 
 import styles from './Search.module.scss'
 
 const Search = () => {
-	const { searchValue, setSearchValue } = useContext(SearchContext)
+	const [value, setValue] = useState('')
+	const { setSearchValue } = useContext(SearchContext)
 	const inputRef = useRef()
 
 	const onClickClear = () => {
 		setSearchValue('')
+		setValue('')
 		inputRef.current.focus()
+	}
+
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	const changeInput = useCallback(
+		debounce(e => {
+			setSearchValue(e)
+		}, 500),
+		[]
+	)
+
+	const onChangeInput = e => {
+		setValue(e.target.value)
+		changeInput(e.target.value)
 	}
 
 	return (
@@ -22,19 +38,18 @@ const Search = () => {
 			>
 				<title />
 				<g id='search'>
-					<path
-						d='M29.71,28.29l-6.5-6.5-.07,0a12,12,0,1,0-1.39,1.39s0,.05,0,.07l6.5,6.5a1,1,0,0,0,1.42,0A1,1,0,0,0,29.71,28.29ZM14,24A10,10,0,1,1,24,14,10,10,0,0,1,14,24Z' />
+					<path d='M29.71,28.29l-6.5-6.5-.07,0a12,12,0,1,0-1.39,1.39s0,.05,0,.07l6.5,6.5a1,1,0,0,0,1.42,0A1,1,0,0,0,29.71,28.29ZM14,24A10,10,0,1,1,24,14,10,10,0,0,1,14,24Z' />
 				</g>
 			</svg>
 			<input
 				ref={inputRef}
-				value={searchValue}
-				onChange={e => setSearchValue(e.target.value)}
+				value={value}
+				onChange={onChangeInput}
 				className={styles.input}
 				type='text'
 				placeholder='Search Pizza'
 			/>
-			{searchValue && (
+			{value && (
 				<svg
 					onClick={onClickClear}
 					className={styles.clearIcon}
@@ -44,8 +59,7 @@ const Search = () => {
 					viewBox='0 0 512 512'
 					width='512px'
 				>
-					<path
-						d='M437.5,386.6L306.9,256l130.6-130.6c14.1-14.1,14.1-36.8,0-50.9c-14.1-14.1-36.8-14.1-50.9,0L256,205.1L125.4,74.5  c-14.1-14.1-36.8-14.1-50.9,0c-14.1,14.1-14.1,36.8,0,50.9L205.1,256L74.5,386.6c-14.1,14.1-14.1,36.8,0,50.9  c14.1,14.1,36.8,14.1,50.9,0L256,306.9l130.6,130.6c14.1,14.1,36.8,14.1,50.9,0C451.5,423.4,451.5,400.6,437.5,386.6z' />
+					<path d='M437.5,386.6L306.9,256l130.6-130.6c14.1-14.1,14.1-36.8,0-50.9c-14.1-14.1-36.8-14.1-50.9,0L256,205.1L125.4,74.5  c-14.1-14.1-36.8-14.1-50.9,0c-14.1,14.1-14.1,36.8,0,50.9L205.1,256L74.5,386.6c-14.1,14.1-14.1,36.8,0,50.9  c14.1,14.1,36.8,14.1,50.9,0L256,306.9l130.6,130.6c14.1,14.1,36.8,14.1,50.9,0C451.5,423.4,451.5,400.6,437.5,386.6z' />
 				</svg>
 			)}
 		</div>
