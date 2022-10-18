b9b2b62b383be3e2b00f1efdee4d7b7a44b53bb3
serhiy hvala <serhiyhvaladew@gmail.com>
2022-10-18 16:43:27 +0300

    Add Error Page and Redux Logic

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,11 @@
-import axios from 'axios'
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { SearchContext } from '../../App'
-import { MOCK_API_ENDPOINT } from '../../constants'
 import { setCategoryId, setCurrentPage } from '../../redux/slices/fliterSlice'
+import { fetchPizzas } from '../../redux/slices/pizzaSlice'
 import Categories from '../Categories/Categories'
+import Error from '../Error/Error'
 import Loader from '../Loader/Loader'
 import Pagination from '../Pagination/Pagination'
 import PizzaBlock from '../PizzaBlock/PizzaBlock'
@@ -13,38 +13,32 @@ import Sort from '../Sort/Sort'
 
 const Home = () => {
 	const { categoryId, sort, currentPage } = useSelector(state => state.filter)
+	const { items, status } = useSelector(state => state.pizza)
 	const { sortType } = sort
 	const dispatch = useDispatch()
 	const loadingPizza = 4
 
 	const { searchValue } = useContext(SearchContext)
-	const [items, setItems] = useState([])
-	const [isLoading, setIsLoading] = useState(true)
-
 	useEffect(() => {
 		window.scrollTo(0, 0)
 	}, [])
 
 	useEffect(() => {
 		const fetchItems = async () => {
-			setIsLoading(true)
-
 			const sortBy = sortType.replace('-', '')
 			const order = sortType.includes('-') ? 'asc' : 'desc'
 			const category = categoryId > 0 ? `category=${categoryId}` : ''
 			const search = searchValue ? `&search=${searchValue}` : ''
 
-			try {
-				const res = await axios.get(
-					`${MOCK_API_ENDPOINT}/items?page=${currentPage}&limit=4&${category}${search}&sortBy=${sortBy}&order=${order}`
-				)
-				setItems(res.data)
-				setIsLoading(false)
-			} catch (error) {
-				console.log(error)
-			} finally {
-				setIsLoading(false)
-			}
+			dispatch(
+				fetchPizzas({
+					sortBy,
+					order,
+					category,
+					search,
+					currentPage
+				})
+			)
 		}
 
 		fetchItems()
@@ -58,24 +52,32 @@ const Home = () => {
 		dispatch(setCurrentPage(number))
 	}
 
+	const loader = [...new Array(loadingPizza)].map((_, index) => (
+		<Loader key={index} />
+	))
+
+	const pizzas = items.map(obj => <PizzaBlock key={obj.id} {...obj} />)
+
 	return (
-		<div className='container'>
-			<div className='content__top'>
-				<Categories value={categoryId} onClickCategory={onClickCategory} />
-				<Sort />
-			</div>
-			<h2 className='content__title'>All Pizzas</h2>
-			<div className='content__items'>
-				{isLoading
-					? [...new Array(loadingPizza)].map((_, index) => (
-							<Loader key={index} />
-					  ))
-					: items.map(obj => <PizzaBlock key={obj.id} {...obj} />)}
-			</div>
-			{categoryId === 0 && (
-				<Pagination currentPage={currentPage} onChangePage={onChangePage} />
+		<>
+			{status === 'error' ? (
+				<Error />
+			) : (
+				<div className='container'>
+					<div className='content__top'>
+						<Categories value={categoryId} onClickCategory={onClickCategory} />
+						<Sort />
+					</div>
+					<h2 className='content__title'>All Pizzas</h2>
+					<div className='content__items'>
+						{status === 'loading' ? loader : pizzas}
+					</div>
+					{categoryId === 0 && (
+						<Pagination currentPage={currentPage} onChangePage={onChangePage} />
+					)}
+				</div>
 			)}
-		</div>
+		</>
 	)
 }
 
