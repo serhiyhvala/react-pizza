ad5e12262df7420b16f070ad64dd7a5c6872c68a
serhiy hvala <serhiyhvaladew@gmail.com>
2022-10-19 13:05:12 +0300

    Create MainLayout For Project With React-Router Outlet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import { createContext, useState } from 'react'
 import { Route, Routes } from 'react-router'
 
 import './assets/styles/app.scss'
 import Cart from './components/Cart/Cart'
-import Header from './components/Header/Header'
+import FullPizza from './components/FullPizza/FullPizza'
 import Home from './components/Home/Home'
 import NotFound from './components/NotFound/NotFound'
+import MainLayout from './layouts/MainLayout'
 
 function App() {
 	return (
-		<div className='wrapper'>
-			<Header />
-			<div className='content'>
-				<Routes>
-					<Route path='/' element={<Home />} />
-					<Route path='/cart' element={<Cart />} />
-					<Route path='*' element={<NotFound />} />
-				</Routes>
-			</div>
-		</div>
+		<Routes>
+			<Route path='/' element={<MainLayout />}>
+				<Route path='' element={<Home />} />
+				<Route path='cart' element={<Cart />} />
+				<Route path='pizza/:id' element={<FullPizza />} />
+				<Route path='*' element={<NotFound />} />
+			</Route>
+		</Routes>
 	)
 }
 
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Outlet } from 'react-router'
+
+import Header from '../components/Header/Header'
+
+const MainLayout = () => {
+	return (
+		<div className='wrapper'>
+			<Header />
+			<div className='content'>
+				<Outlet />
+			</div>
+		</div>
+	)
+}
+
+export default MainLayout
